87f6024f8dc42eae79a4a20a41166025276c810e
roxlu <diederick@apollomedia.nl>
2010-12-14 00:00:40 +0100

    Working on getting libusb to compile on Mac

--- CMakeModules/Findlibusb-1.0.cmake
+++ CMakeModules/Findlibusb-1.0.cmake
@@ -17,11 +17,11 @@
 #
 
 
-if (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
+if (USB_LIB_NAME AND USB_INCLUDE_DIR)
   # in cache already
-  set(LIBUSB_FOUND TRUE)
-else (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
-  find_path(LIBUSB_1_INCLUDE_DIR
+  set(USB_LIB_FOUND TRUE)
+else (USB_LIB_NAME AND USB_INCLUDE_DIR)
+  find_path(USB_INCLUDE_DIR
     NAMES
 	libusb-1.0/libusb.h
     PATHS
@@ -33,7 +33,7 @@ else (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
 	  libusb-1.0
   )
 
-  find_library(LIBUSB_1_LIBRARY
+  find_library(USB_LIB_NAME
     NAMES
       usb-1.0
     PATHS
@@ -43,30 +43,31 @@ else (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
       /sw/lib
   )
 
-  set(LIBUSB_1_INCLUDE_DIRS
-    ${LIBUSB_1_INCLUDE_DIR}
-  )
-  set(LIBUSB_1_LIBRARIES
-    ${LIBUSB_1_LIBRARY}
-)
+  
+  #set(USB_INCLUDE_DIR
+  #  ${USB_INCLUDE_DIR}
+  #)
+  #set(USB_LIB_NAME
+  #  ${USB_LIB_NAME}
+#)
 
-  if (LIBUSB_1_INCLUDE_DIRS AND LIBUSB_1_LIBRARIES)
-     set(LIBUSB_1_FOUND TRUE)
-  endif (LIBUSB_1_INCLUDE_DIRS AND LIBUSB_1_LIBRARIES)
+  if (USB_INCLUDE_DIR AND USB_LIB_NAME)
+     set(USB_LIB_FOUND TRUE)
+  endif (USB_INCLUDE_DIR AND USB_LIB_NAME)
 
-  if (LIBUSB_1_FOUND)
-    if (NOT libusb_1_FIND_QUIETLY)
-      message(STATUS "Found libusb-1.0:")
-	  message(STATUS " - Includes: ${LIBUSB_1_INCLUDE_DIRS}")
-	  message(STATUS " - Libraries: ${LIBUSB_1_LIBRARIES}")
-    endif (NOT libusb_1_FIND_QUIETLY)
-  else (LIBUSB_1_FOUND)
-    if (libusb_1_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find libusb")
-    endif (libusb_1_FIND_REQUIRED)
-  endif (LIBUSB_1_FOUND)
+  #if (USB_LIB_FOUND)
+  #  if (NOT libusb_1_FIND_QUIETLY)
+  #    message(STATUS "Found libusb-1.0:")
+#	  message(STATUS " - Includes: ${LIBUSB_1_INCLUDE_DIRS}")
+#	  message(STATUS " - Libraries: ${LIBUSB_1_LIBRARIES}")
+ #   endif (NOT libusb_1_FIND_QUIETLY)
+  #else (LIBUSB_1_FOUND)
+   # if (libusb_1_FIND_REQUIRED)
+    #  message(FATAL_ERROR "Could not find libusb")
+    #endif (libusb_1_FIND_REQUIRED)
+  #endif (LIBUSB_1_FOUND)
 
   # show the LIBUSB_1_INCLUDE_DIRS and LIBUSB_1_LIBRARIES variables only in the advanced view
-  mark_as_advanced(LIBUSB_1_INCLUDE_DIRS LIBUSB_1_LIBRARIES)
+  #mark_as_advanced(LIBUSB_1_INCLUDE_DIRS LIBUSB_1_LIBRARIES)
 
-endif (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
+endif ()
